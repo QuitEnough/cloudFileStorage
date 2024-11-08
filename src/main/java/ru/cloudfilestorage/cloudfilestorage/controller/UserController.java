1da58340e5fa673983d954ab2b77b000c9ff2d85
TestingMyself0 <workspace01001@gmail.com>
2024-11-08 14:07:47 +0300

    deleted unused statement

--- src/main/java/ru/cloudfilestorage/cloudfilestorage/controller/UserController.java
+++ src/main/java/ru/cloudfilestorage/cloudfilestorage/controller/UserController.java
@@ -2,14 +2,12 @@ package ru.cloudfilestorage.cloudfilestorage.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import ru.cloudfilestorage.cloudfilestorage.domain.dto.UserCreateRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.cloudfilestorage.cloudfilestorage.domain.dto.UserResponse;
-import ru.cloudfilestorage.cloudfilestorage.domain.entity.User;
 import ru.cloudfilestorage.cloudfilestorage.service.impl.UserServiceImpl;
 
 @RestController
