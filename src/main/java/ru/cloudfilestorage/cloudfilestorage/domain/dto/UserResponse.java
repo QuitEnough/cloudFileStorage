d144865ac7b2ab543cc0f3ee85b33688823bd638
Yana <myemail@gmail.com>
2024-11-11 16:35:52 +0300

    removed unused statements

--- src/main/java/ru/cloudfilestorage/cloudfilestorage/domain/dto/UserResponse.java
+++ src/main/java/ru/cloudfilestorage/cloudfilestorage/domain/dto/UserResponse.java
@@ -1,9 +1,6 @@
 package ru.cloudfilestorage.cloudfilestorage.domain.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
