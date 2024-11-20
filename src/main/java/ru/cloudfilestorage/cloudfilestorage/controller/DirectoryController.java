031f9a1bdfddef9415b26591f0b129a15e26e91f
TestingMyself0 <workspace01001@gmail.com>
2024-11-20 14:34:33 +0300

    deleted unused statements

--- src/main/java/ru/cloudfilestorage/cloudfilestorage/controller/DirectoryController.java
+++ src/main/java/ru/cloudfilestorage/cloudfilestorage/controller/DirectoryController.java
@@ -1,6 +1,5 @@
 package ru.cloudfilestorage.cloudfilestorage.controller;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
