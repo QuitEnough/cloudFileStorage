06d830a389ae6015f9ed20dd6351f984a18e9ba7
TestingMyself0 <workspace01001@gmail.com>
2024-11-26 12:16:01 +0300

    ELK added

--- src/main/java/ru/cloudfilestorage/cloudfilestorage/config/ElasticsearchClientConfiguration.java
+++ src/main/java/ru/cloudfilestorage/cloudfilestorage/config/ElasticsearchClientConfiguration.java
@@ -7,23 +7,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+//import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "com.example.elasticsearch.repository")
-@ComponentScan(basePackages = { "com.example.elasticsearch" })
-public class ElasticsearchClientConfig extends AbstractElasticsearchConfiguration {
-
-    @Override
-    @Bean
-    public RestHighLevelClient elasticsearchClient() {
-
-        RestClientBuilder builder = RestClient.builder(
-                        new HttpHost("localhost", 9200))
-                .setRequestConfigCallback(
-                        requestConfigBuilder -> requestConfigBuilder
-                                .setConnectionRequestTimeout(0));
-
-        return new RestHighLevelClient(builder);
-    }
+//@EnableElasticsearchRepositories(basePackages = "com.example.elasticsearch.repository")
+//@ComponentScan(basePackages = { "com.example.elasticsearch" })
+public class ElasticsearchClientConfiguration {
+//public class ElasticsearchClientConfiguration extends AbstractElasticsearchConfiguration {
+//
+//    @Override
+//    @Bean
+//    public RestHighLevelClient elasticsearchClient() {
+//
+//        RestClientBuilder builder = RestClient.builder(
+//                        new HttpHost("localhost", 9200))
+//                .setRequestConfigCallback(
+//                        requestConfigBuilder -> requestConfigBuilder
+//                                .setConnectionRequestTimeout(0));
+//
+//        return new RestHighLevelClient(builder);
+//    }
 
 }
