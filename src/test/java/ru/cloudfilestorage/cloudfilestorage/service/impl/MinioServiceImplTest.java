350d1502ff167ea9d671db8e78d3e2f6a25d238e
Yana <myemail@gmail.com>
2024-12-03 16:16:43 +0300

    test

--- src/test/java/ru/cloudfilestorage/cloudfilestorage/service/impl/MinioServiceImplTest.java
+++ src/test/java/ru/cloudfilestorage/cloudfilestorage/service/impl/MinioServiceImplTest.java
@@ -1,21 +1,45 @@
 package ru.cloudfilestorage.cloudfilestorage.service.impl;
 
+import io.minio.GetObjectResponse;
 import io.minio.MinioClient;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
-import static org.mockito.Mockito.mock;
+import java.io.InputStream;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class MinioServiceImplTest {
 
-    private final String bucket = mock(String.class);
+    private final String bucket = "files-bucket";
 
     private final MinioClient minioClient = mock(MinioClient.class);
 
     private final MinioServiceImpl minioService = new MinioServiceImpl(bucket, minioClient);
 
     @Test
-    void fileSavedSuccess() {
+    void fileSavedSuccess() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("file", "test".getBytes());
+        minioService.save(uuid, mockMultipartFile);
+        verify(minioClient).putObject(any());
 
+    }
+
+    @Test
+    void fileDeletedSuccess() throws Exception {
+        minioService.delete(UUID.randomUUID());
+        verify(minioClient).removeObject(any());
+    }
+
+    @Test
+    void fileFoundSuccess() throws Exception {
+        minioService.find(UUID.randomUUID());
+        verify(minioClient).getObject(any());
     }
 
 }
